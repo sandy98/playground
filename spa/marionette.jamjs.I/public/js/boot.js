9229d690077280fec828cf179ef5f3b2b84a5d8b
Domingo Ernesto Savoretti <esavoretti@gmail.com>
2013-04-18 06:42:17 -0300

    modified boot.js

--- spa/marionette.jamjs.I/public/js/boot.js
+++ spa/marionette.jamjs.I/public/js/boot.js
@@ -1,9 +1,6 @@
 
 require(["cs!app/app"], function(App) {
-    var app = new App();
-    app.initialize();
-    return app;
-    //var app = {version: '0.01'};
-    //var app = App0;
-    //console.log("App version is " + app.version + ".");
+    window.app = new App();
+    window.app.initialize();
+    return window.app;
     });
